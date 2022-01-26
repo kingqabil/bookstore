56728cc340b3100d4280cebf4c067dae00f8010f
kingqabil <kingqabil@live.com>
2022-01-26 18:16:12 +0300

    declared initialstate to Empty array

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,12 +1,14 @@
 const BOOK_ADDED = 'bookStore/books/BOOK_ADDED';
 const BOOK_REMOVED = 'bookStore/books/BOOK_REMOVED';
 
-export const addBook = payload => ({
+export const addBook = (payload) => ({
   type: BOOK_ADDED,
   payload,
 });
 
-export const removeBook = payload => ({
+export const removeBook = (payload) => ({
   type: BOOK_REMOVED,
   payload,
 });
+
+const initialState = [];
