c6d53e24c1330a00d638757234a0448c552785aa
kingqabil <kingqabil@live.com>
2022-01-26 20:31:12 +0300

    class to handle fetch methods

---
+++ src/utils/fetchwrapper.js
@@ -0,0 +1,33 @@
+export default class FetchWrapper {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+
+  async get(endpoint) {
+    const response = await fetch(this.baseURL + endpoint);
+    return response.json();
+  }
+
+  async post(endpoint, data) {
+    const response = await fetch(this.baseURL + endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(data),
+    });
+    return response;
+  }
+
+
+  async delete(endpoint) {
+    const response = await fetch(this.baseURL + endpoint, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+    return response;
+  }
+}
